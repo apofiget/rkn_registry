342bb7ceb6c3b751ede2002c0dcb567df564ed25
Andrey Andruschenko <aag@soskol.com>
2014-04-17 17:05:46 +0400

    Document functions.

--- src/blacklist.erl
+++ src/blacklist.erl
@@ -2,12 +2,26 @@
 
 -export([load_csv/1, jnx_set_route/2, jnx_del_route/2]).
 
+%% @spec load_csv(F :: list()) -> {ok, List} | {error, reason}
+%% @doc Open file file F and load list from it. String separator is ';'
+%% @end
 load_csv(File) when is_list(File) ->
     case file:open(File, [read]) of
     	{ok, Dev} -> parse_list(Dev,[]);
     	{error, Reason} -> {error, Reason}
     end.
 
+%% @spec jnx_set_route(F :: list(), L :: list()) -> {ok, List} | {error, reason}
+%% @doc Make Juniper config to file F with set route commands.
+%% @end
+jnx_set_route(F, L) -> jnx_route("set", F, L).
+
+%% @spec jnx_del_route(F :: list(), L :: list()) -> {ok, List} | {error, reason}
+%% @doc Make Juniper config to file F with delete route commands.
+%% @end
+jnx_del_route(F, L) -> jnx_route("delete", F, L).
+
+%% @hidden
 parse_list(Dev, Acc) ->
 	case file:read_line(Dev) of 
 		eof -> file:close(Dev), {ok, Acc};
@@ -26,15 +40,13 @@ parse_list(Dev, Acc) ->
 			end
 	end.
 
-jnx_set_route(F, L) -> jnx_route("set", F, L).
-jnx_del_route(F, L) -> jnx_route("delete", F, L).
-
+%% @hidden
 jnx_route(Prefix, File, L) ->
 	case file:open(File, [write]) of
 		{ok, Dev} -> make_jnx_route_list(Prefix, Dev, L);
 		{error, Reason} -> {error, Reason}
 	end. 
-
+%% @hidden
 make_jnx_route_list(_Prefix, Dev, []) -> file:close(Dev);
 
 make_jnx_route_list(Prefix, Dev, [H|T]) ->
