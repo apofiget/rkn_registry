0cdd988085e5f0424234e6385fcbf30318f37f51
Andrey Andruschenko <aag@soskol.com>
2014-07-21 13:55:20 +0400

    Готовимся к новому формату реестра.

--- include/registry.hrl
+++ include/registry.hrl
@@ -2,7 +2,7 @@
 			{xml, "priv/request.xml"},
 			{sign, "priv/request.bin"},
 			{trace, "priv/logs/updates.log"},
-			{doc_root, file:get_cwd()},
+			{doc_root, "./"},
 			{hostname, "localhost"},
 			{listen_on, "127.0.0.1"},
 			{get_last_update_period, 600},
@@ -14,6 +14,11 @@
 		]).
 
 -define(REG_SRV_URL,"http://vigruzki.rkn.gov.ru/services/OperatorRequest/?wsdl").
+
+-define(REG_TYPE, [{1,<<"Реестр ЕАИС"/utf8>>}, {2, <<"Реестр НАП"/utf8>>}, 
+	{3, <<"Реестр 398-ФЗ"/utf8>>}, {4, <<"Реестр 97-ФЗ, организаторы распространения информации"/utf8>>}]).
+
+get_reg_type(Id) -> proplists:get_value(Id, ?REG_TYPE, <<"Неизвестный тип реестра"/utf8>>).
 
 get_option(Key) ->
     case application:get_env(Key) of
