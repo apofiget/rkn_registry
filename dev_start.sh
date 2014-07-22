670906e5ba98522e73d572f7c1f8376faeab3d91
Andrey Andruschenko <aag@soskol.com>
2014-07-22 16:57:14 +0400

    Изменены скрипты запуска

--- dev_start.sh
+++ dev_start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-erl +pc unicode -pa ebin/ deps/*/ebin/ -config app.config -sname some_for_work -eval 'application:start(rkn_registry)'
+erl +pc unicode -pa ebin/ deps/*/ebin/ -config app.config -sname rkn_registry -eval 'application:start(rkn_registry)'
